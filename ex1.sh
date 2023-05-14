3a38a86a8f420ca50086f5a30dd1021851b42f75
Noam Mirjani <Noamirjani@gmail.com>
2023-05-14 16:07:44 +0300

    ex1

---
+++ ex1.sh
@@ -0,0 +1,34 @@
+# ----- ex1 ------
+# 1
+echo Noam > ~/a.txt
+#second option:
+cd ~
+touch b.txt
+echo "Noam" > b.txt
+
+# 2
+mkdir new
+
+# 3
+cp a.txt new/a.txt
+
+# 4
+mv new/a.txt new/noam.txt 
+
+# 5
+gedit new/noam.txt
+
+# 6
+cp new/a.txt new/newFile.txt
+
+# 7
+cd new;
+ls
+
+# 8
+cd ../
+#second option:
+cd ~
+
+# 9
+rm -r new
